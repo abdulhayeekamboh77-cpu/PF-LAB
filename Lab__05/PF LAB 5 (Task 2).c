cd5c84602f17d37e5f772a48be998f36f2b28d51
Abdul Hayee Kamboh <abdulhayeekamboh77@gmail.com>
2026-09-23 12:21:35 +0500

    Create PF LAB 5 (Task 2).c

---
+++ Lab__05/PF LAB 5 (Task 2).c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+int main(){
+	// Enter Marks Of The Following Subject //
+	int Programming, Mathematics, AI, Attendance ;
+	float Average;
+	printf("Enter the Marks Of Programming, Mathematic, AI and Attendance: ");
+	scanf("\n%d \n%d \n%d \n%d", &Programming, &Mathematics, &AI, &Attendance);
+	printf("\nThe Marks Of Programming Is: %d", Programming);
+	printf("\nThe Marks Of Mathematic Is: %d", Mathematics);
+	printf("\nThe Marks Of AI Is: %d", AI);
+	printf("\nThe Marks Of Attendance Is: %d", Attendance);
+	if (Programming >= 50){
+	    printf("\nStudent Is Eligible");
+	}
+	else{
+	    printf("\nNot Eligible");
+	    return 0;
+	}
+	if (Mathematics >= 50){
+	    printf("\nStudent Is Eligible");
+	}
+	else{
+	    printf("\nNot Eligible");
+	    return 0;  
+	}
+	if (AI >= 50){
+	    printf("\nStudent Is Eligible");
+	}
+	else{
+	    printf("\nNot Eligible");
+	    return 0; 
+	}
+	if (Attendance >= 75){
+	    printf("\nStudent Is Eligible");
+	}
+	else{
+	    printf("\nNot Eligible");
+	    return 0; 
+	} 
+	// Calculating Average //
+	Average = (Mathematics + Programming + AI)/3;
+	printf("\nAverage = %2f%%", Average);
+	if (Average >= 80){
+	    printf("\nExcellent Performance");
+	}
+	else if (Average >= 70){
+	    printf("\nVery Good Performance");
+	}
+	else if (Average >= 60){
+	    printf("\nGood Performance");
+	}
+	else if (Average >= 50){
+	    printf("\nSatisfactory Performance");
+	}
+	else if (Average < 50){
+	    printf("\nPoor Performance");
+	}
+    return 0;
+}
